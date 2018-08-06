b4525f2e81a21c5dfc0b8c6e84c02dfe7a6c8db1
Maximo <mdeleon@vivial.net>
2018-08-06 16:48:49 -0400

    add i3 and polybar base config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# display bar in all monitors
+if type "xrandr"; then
+  for m in $(xrandr --query | grep -w "connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar1 &
+  done
+else
+  polybar --reload bar1 &
+fi
+# Launch bar1 and bar2
+# polybar bar1 # &
+#polybar bar2 &
+
+echo "Bars launched..."
