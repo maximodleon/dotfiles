804343ae5ec22242d39166670589416b0f0c1233
Maximo <mdeleon@vivial.net>
2018-08-09 13:19:52 -0400

    add new polybar config and weather widget

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,16 +5,19 @@ killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 # display bar in all monitors
 if type "xrandr"; then
   for m in $(xrandr --query | grep -w "connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload bar1 &
+    MONITOR=$m polybar --reload bar2 &
   done
 else
   polybar --reload bar1 &
+  polybar --reload bar2 &
 fi
 # Launch bar1 and bar2
 # polybar bar1 # &
-#polybar bar2 &
+# polybar bar2 &
 
 echo "Bars launched..."
