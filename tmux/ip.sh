a2ef9032c7db306773c9e5694da376aed8ee2849
Máximo De León <maximodleon@gmail.com>
2020-01-16 20:33:05 -0500

    update files to get ip and ssid to be cross-platform

---
+++ tmux/ip.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+OS=$(uname)
+MAC="Darwin"
+WIFI_IP=""
+ETH_IP=""
+SSID=""
+
+if [ "$OS" = "$MAC" ]
+then
+  WIFI_IP=$(netstat -in | rg en0 | awk '{print $4}' | rg '([0-9]{1,3}[\.]){3}[0-9]{1,3}' || echo 'Disconnected')
+  ETH_IP=$(netstat -in | rg en4 | awk '{print $4}' | rg '([0-9]{1,3}[\.]){3}[0-9]{1,3}' || echo 'Disconnected')
+else
+  WIFI_IP=$(ip addr show dev wlp3s0 | rg -m1 'inet' | rg '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | awk -F '/' '{print $1}' | awk '{print $2}' || echo 'Disconnected') 
+  ETH_IP=$(ip addr show dev enp0s25 | rg -m1 'inet' || echo '0.0.0.0' | rg '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | awk -F '/' '{print $1}' | awk '$1 == "0.0.0.0" {print "Disconnected"; next} {print $2}') 
+fi
+
+if [ "$OS" = "$MAC" ]
+then
+  SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F: '/ SSID/ {print $2}' || echo 'Offline')
+else
+  SSID=$(iw dev wlp3s0 info | rg 'ssid' | awk '{print $2}')
+fi
+
+
+echo " $SSID|$WIFI_IP #[fg=#454b68,bg=#2e3244,nobold,nounderscore,noitalics] #[fg=#c6c8d1,bg=#2e3244]  $ETH_IP #[fg=#454b68,bg=#2e3244,nobold,nounderscore,noitalics]" 
--- tmux/ssid.sh
+++ tmux/ssid.sh
@@ -7,7 +7,7 @@ if [ "$OS" = "$MAC" ]
 then
   SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F: '/ SSID/ {print $2}' || echo 'Offline')
 else
-  echo "$OS"
+  SSID=$(iw dev wlp3s0 info | rg 'ssid' | awk '{print $2}')
 fi
 
 echo $SSID
