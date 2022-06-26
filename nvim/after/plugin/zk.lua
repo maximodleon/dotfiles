c76491c0dca613b3144cdb8bd14ccd0f91b66cce
Maximo De Leon <maximodleon@gmail.com>
2022-06-26 19:39:32 -0400

    Maps de los tipos de notas

--- nvim/after/plugin/zk.lua
+++ nvim/after/plugin/zk.lua
@@ -12,3 +12,12 @@ api.nvim_set_keymap("n", "<leader>zt", "<Cmd>ZkTags<CR>", opts)
 api.nvim_set_keymap("n", "<leader>zn", "<Cmd>ZkNotes { sort = { 'modified' }, match = vim.fn.input('Search: ') }<CR>", opts)
 
 api.nvim_set_keymap("v", "<leader>zf", ":'<,'>ZkMatch<CR>", opts)
+
+-- Crear nota permanente
+api.nvim_set_keymap("n", "<leader>zp", "<Cmd>ZkNew { title = vim.fn.input('Title: '), dir='Notas Permanentes' }<CR>", opts)
+-- Crear nota fugaz
+api.nvim_set_keymap("n", "<leader>zf", "<Cmd>ZkNew { title = vim.fn.input('Title: '), dir='Notas Fugaces' }<CR>", opts)
+-- Crear nota literaria
+api.nvim_set_keymap("n", "<leader>zl", "<Cmd>ZkNew { title = vim.fn.input('Title: '), dir='Notas Literarias' }<CR>", opts)
+-- Crear nota referencia
+api.nvim_set_keymap("n", "<leader>zr", "<Cmd>ZkNew { title = vim.fn.input('Title: '), dir='Notas Referencias' }<CR>", opts)
