00b61c4c88928578f927d78295f0b1d51e535aed
Maximo De Leon <maximodleon@gmail.com>
2022-07-01 16:13:26 -0400

    Agregar luasnips para markdown

--- nvim/lua/user/snips.lua
+++ nvim/lua/user/snips.lua
@@ -1,9 +1,39 @@
 local ls = require("luasnip")
 local snip = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
+local rep = require("luasnip.extras").rep
+
+ls.config.set_config {
+	history = true,
+	updateevents = "TextChanged,TextChangedI"
+
+}
 
 ls.add_snippets(nil, {
-	all = {
-		snip({ trig="snippet", name="snippet" }, { t({ "AA"})})
+	markdown = {
+		snip({ trig="literaria", name="Nota Literaria" }, 
+			{ 
+			t({"----", "title: "}), i(1, "note_title"),
+			t({"", "autor: "}), i(2, "autor"),
+			t({"", "tipo: "}), i(3, "tipo"),
+			t({"", "URL: "}), i(4, "url"),
+			t({"", "tags: "}), i(5, "tags"),
+			t({"", "topics: "}), i(6, "topics"),
+			t({"", "---"}),
+			t({"", "# "}), rep(1),
+			i(0)
+		}),
+		snip({ trig="permanente", name="Nota Permanente" }, 
+			{ 
+			t({"----", "title: "}), i(1, "note_title"),
+			t({"", "reference note: "}), i(2, "refnote"),
+			t({"", "fleeting notes: "}), i(3, "fleetnote"),
+			t({"", "---"}),
+			t({"", "# "}), rep(1),
+			t({"", ""}),
+			t({"", "# Link"}),
+			i(0)
+		}),
 	}
 })
