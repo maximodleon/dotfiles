e30b42d5a62f714b5911549433b130181feb2dbf
Maximo De Leon <maximodleon@gmail.com>
2023-03-18 13:13:08 -0400

    More mappings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,12 +1,16 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local on_attach = function(client, bufnr) 
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=bufnr})
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=bufnr})
-	vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, {buffer=bufnr})
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=bufnr})
-	vim.keymap.set("n", " df", vim.diagnostic.goto_next, {buffer=bufnr})
-	vim.keymap.set("n", " dp", vim.diagnostic.goto_prev, {buffer=bufnr})
+	vim.keymap.set("n", "H", vim.lsp.buf.hover, {buffer=bufnr, desc='Hover'})
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=bufnr, desc='[G]o to [D]efinition'})
+	vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, {buffer=bufnr, desc='[G]o to [T]ype definition'})
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=bufnr, desc='[G]o to [I]mplementation'})
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer=bufnr, desc='[R]e[n]ame '})
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer=bufnr, desc='[Code] [A]ction'})
+	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {buffer=bufnr, desc='Go to next diagnostic message'})
+	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {buffer=bufnr, desc='Go to prev diagnostic message'})
+	vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, {buffer=bufnr, desc='Open diagnostics list'})
+	vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {buffer=bufnr, desc='Open floating diagnostics message'})
 end
 
 -- Configure ts language server
