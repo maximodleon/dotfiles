7dd58167d9bb240e12eeb6dd805c379c9c3bc0be
Maximo De Leon <maximodleon@gmail.com>
2025-01-11 11:47:48 -0500

    update config. add ghostty config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,7 @@
 require("config.lazy")
 
+vim.api.nvim_set_keymap("n", ";", ":", { noremap = true })
+vim.api.nvim_set_keymap("n", ":", ";", { noremap = true })
+
 vim.opt.number = true
 vim.opt.relativenumber = true
