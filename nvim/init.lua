bcde44ea972c107baf8a4fad031992fd45bc09bf
Maximo De Leon <maximodleon@gmail.com>
2025-01-11 20:47:53 -0500

    update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,11 @@
 require("config.lazy")
 
+require('mason').setup()
+require('mason-lspconfig').setup( {
+	ensure_installed = { 'ts_ls' },
+	automatic_installation = true
+})
+
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = true })
 vim.api.nvim_set_keymap("n", ":", ";", { noremap = true })
 
--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
    build = ":TSUpdate",
    config = function()
 	   require'nvim-treesitter.configs'.setup {
-		   ensure_installed = { "c", "lua", "elixir", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+		   ensure_installed = { "c", "lua", "elixir", "vim", "vimdoc", "query", "markdown", "markdown_inline", "ocaml" },
 
 		   highlight = {
 			   enable = true,
