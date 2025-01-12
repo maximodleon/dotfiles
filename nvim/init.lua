a75a23f4e3b61361e83ad82e363a5b1bb49a11ae
Maximo De Leon <maximodleon@gmail.com>
2025-01-11 20:55:38 -0500

    add telescope config

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,5 +9,13 @@ require('mason-lspconfig').setup( {
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = true })
 vim.api.nvim_set_keymap("n", ":", ";", { noremap = true })
 
+-- Telescope mappings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+
 vim.opt.number = true
 vim.opt.relativenumber = true
