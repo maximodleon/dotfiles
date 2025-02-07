b70e61fab59ba8785449cbdb58e1117e95e28918
Maximo De Leon <maximodleon@gmail.com>
2025-02-06 20:28:08 -0500

    Fix lsp and tabs

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,22 +27,40 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.tabstop=2
+vim.opt.softtabstop=2
+vim.opt.shiftwidth=2
+vim.opt.expandtab=true
 
 -- LSP
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local on_attach = function(client, bufnr)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=bufnr, desc='[G]o to [D]efinition'})
+vim.keymap.set("n", "H", vim.lsp.buf.hover, {buffer=bufnr, desc='Hover'})
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer=bufnr, desc='[R]e[n]ame '})
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, {buffer=bufnr, desc='Open diagnostics list'})
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {buffer=bufnr, desc='Open floating diagnostics message'})
+	
+  
+end
+
 require ('lspconfig')['ts_ls'].setup {
-	capabilities = capabilities
+	capabilities = capabilities,
+	on_attach = on_attach
 }
-
--- Configure lua ls
 require ('lspconfig')['lua_ls'].setup {
-	capabilities = capabilities
+	capabilities = capabilities,
+	on_attach = on_attach
 }
-
--- Configure Go
 require ('lspconfig')['gopls'].setup {
-	capabilities = capabilities
+	capabilities = capabilities,
+	on_attach = on_attach
 }
 require ('lspconfig')['zls'].setup {
-	capabilities = capabilities
+	capabilities = capabilities,
+	on_attach = on_attach
+}
+require ('lspconfig')['clangd'].setup {
+	capabilities = capabilities,
+	on_attach = on_attach
 }
