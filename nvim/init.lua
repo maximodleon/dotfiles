5937035515077a88c72da1871c719c5c8d5b87ed
Maximo De Leon <maximodleon@gmail.com>
2025-02-06 20:32:35 -0500

    Add more LSPs

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,14 +34,13 @@ vim.opt.expandtab=true
 
 -- LSP
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 local on_attach = function(client, bufnr)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=bufnr, desc='[G]o to [D]efinition'})
 vim.keymap.set("n", "H", vim.lsp.buf.hover, {buffer=bufnr, desc='Hover'})
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer=bufnr, desc='[R]e[n]ame '})
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, {buffer=bufnr, desc='Open diagnostics list'})
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {buffer=bufnr, desc='Open floating diagnostics message'})
-	
-  
 end
 
 require ('lspconfig')['ts_ls'].setup {
@@ -61,6 +60,21 @@ require ('lspconfig')['zls'].setup {
 	on_attach = on_attach
 }
 require ('lspconfig')['clangd'].setup {
+	capabilities = capabilities,
+	on_attach = on_attach
+}
+
+require ('lspconfig')['denols'].setup {
+	capabilities = capabilities,
+	on_attach = on_attach
+}
+
+require ('lspconfig')['elixirls'].setup {
+	capabilities = capabilities,
+	on_attach = on_attach
+}
+
+require ('lspconfig')['ocamllsp'].setup {
 	capabilities = capabilities,
 	on_attach = on_attach
 }
