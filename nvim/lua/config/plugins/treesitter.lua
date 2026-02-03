20ca975e93051514c7019b41596a645e97e64adb
Maximo De Leon <maximodleon@gmail.com>
2026-02-03 14:48:35 -0500

    add opencode config

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -1,10 +1,12 @@
  return {
    "nvim-treesitter/nvim-treesitter",
+   tag = "v0.10.0",
+   lazy = false,
    build = ":TSUpdate",
    config = {
-                  ensure_installed = { "c", "lua", "elixir", "vim", "vimdoc", "query", "markdown", "markdown_inline", "ocaml" },
-                  highlight = {
-                          enable = true,
-                  }
+                  ensure_installed = { "c", "lua", "elixir", "vim", "vimdoc", "query", "markdown", "markdown_inline", "ocaml", "blade" },
+                  highlight = { enable = true },
+                  sync_install = false,
+                  indent = { enable = false },
           }
 }
